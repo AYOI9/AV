27337085ca2bce940c18d90d7f080ccce6a5b766
maior <maior@DESKTOP-FHGLER9>
2024-11-09 15:59:41 +0200

    Добавьте файлы проекта.

---
+++ AV/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("Введите значение t: ");
+    double t = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение y: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+    double Z = (2 * t + y * Math.Cos(t)) / (Math.Sqrt(y) + 4.831);
+    Console.WriteLine($"Значение Z: {Z}");
+}
+catch
+{
+    Console.WriteLine("Введите верное значение:");
+}
+        
